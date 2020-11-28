4470c46f29c75966fb512be1a3d31ef98129f595
rsievers82 <rsievers@live.unc.edu>
2020-11-27 21:34:45 -0500

    Start autocomplete

--- react-app/src/leaderboard.js
+++ react-app/src/leaderboard.js
@@ -1,18 +1,41 @@
 // import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Login } from './login';
 
 export function Leaderboard(props) {
+    const [input, setInput] = useState('');
+
+    const [filteredUsers, setFilteredUsers] = useState([]);
+
     const players = props.players;
+    const usernames = players.map(player => player.user).sort();
+
 
     function handleLoginButtonClick(event) {
         ReactDOM.render(<Login />, document.getElementById('root'));
     }
 
-    const tableRows = players.map(player => {
+    function handleInput(event) {
+        setInput(event.target.value);
+        setFilteredUsers(usernames.filter(user => user.startsWith(event.target.value)));
+    }
+
+    function handlePlayerPick(event) {
+        tableRows = players.slice(players.findIndex(player => player.user === event.target.key))
+    }
+
+    const usersFormatted = filteredUsers.map(filteredUser => {
         return (
-            <tr>
+            <div key={filteredUser}>
+                {filteredUser}
+            </div>
+        )
+    })
+
+    let tableRows = players.map(player => {
+        return (
+            <tr key={player.user}>
                 <td>
                     {players.findIndex(p => p.user === player.user) + 1}
                 </td>
@@ -29,6 +52,12 @@ export function Leaderboard(props) {
 
     return (
         <div>
+            <div className="search">
+                <input name="search" value={input} id="search" type="text" placeholder="search for a player..." onChange={handleInput}/>
+                <div className="options" onClick={handlePlayerPick}>
+                    {usersFormatted}
+                </div>
+            </div>
             <table className="table table-hover">
                 <thead className='thead-light'>
                     <tr>
